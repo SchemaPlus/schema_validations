3b312b1dcdbf7b9f16681c4bc35c697e7c5dbd2a
ronen barzel <ronen@barzel.org>
2011-07-29 22:23:34 -0700

    conneucion tweaks:  name "schema_validations" not "schema_plus"; use sqlite3 PRAGMA synchronous = OFF for quicker testing.

--- spec/connection.rb
+++ spec/connection.rb
@@ -3,11 +3,12 @@ require 'logger'
 ActiveRecord::Base.logger = Logger.new(File.open("sqlite3.log", "w"))
 
 ActiveRecord::Base.configurations = {
-  'schema_plus' => {
+  'schema_validations' => {
     :adapter => 'sqlite3',
-    :database => File.expand_path('schema_plus.sqlite3', File.dirname(__FILE__)),
+    :database => File.expand_path('schema_validations.sqlite3', File.dirname(__FILE__)),
   }
 
 }
 
-ActiveRecord::Base.establish_connection 'schema_plus'
+ActiveRecord::Base.establish_connection 'schema_validations'
+ActiveRecord::Base.connection.execute "PRAGMA synchronous = OFF"
